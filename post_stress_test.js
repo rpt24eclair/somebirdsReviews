134b32ae6d115ae607072943db5351e9dba82aaf
Meyvi <meyvim@gmail.com>
2021-02-25 18:33:40 -0800

    Adds post stress test

--- post_stress_test.js
+++ post_stress_test.js
@@ -1,12 +1,22 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-export let options = {
-  vus: 1000,
-  duration: '30s',
-};
-
 export default function () {
-  http.get('http://localhost:3003/shoes/9056785/reviews');
-  sleep(1);
-}
+  var url = 'http://localhost:3003/shoes/9078345/reviews';
+  var payload = JSON.stringify({
+      name: "Meyvi",
+      headline: "Posting",
+      review: "Blah!",
+      rating: 5,
+      fit_feedback: -1,
+      shoe_id: 9078345
+  });
+
+  var params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  http.post(url, payload, params);
+};
